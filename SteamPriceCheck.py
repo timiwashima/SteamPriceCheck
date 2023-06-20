29e939e2649c0cdf75e6488e099f11be9fa41af2
timiwashima <52045865+timiwashima@users.noreply.github.com>
2023-06-20 13:55:28 -0700

    Update SteamPriceCheck.py
    Added additional games, with one currently successful for a lower price.
    
    Need to modify to automate more and require less manual entry, especially for the gameName and price checks

--- SteamPriceCheck.py
+++ SteamPriceCheck.py
@@ -8,16 +8,35 @@ Created on Tue Jun 20 13:08:29 2023
 import requests
 from bs4 import BeautifulSoup
 
-request=requests.get('https://store.steampowered.com/app/1364780/Street_Fighter_6/')
+games={
+       'Street Fighter 6':'https://store.steampowered.com/app/1364780/Street_Fighter_6/',
+       'Monster Hunter Rise':'https://store.steampowered.com/app/1446780/MONSTER_HUNTER_RISE/',
+       'Kingdoms and Castles':'https://store.steampowered.com/app/569480/Kingdoms_and_Castles/'
+       }
 
-content=request.content
-soup=BeautifulSoup(content, 'html.parser')
-element=soup.find('div',{'class':'game_purchase_price price'})
+for game,url in games.items():
+    gameName=game
+    gameURL=url
+    
+    request=requests.get(gameURL)
+    content=request.content
+    soup=BeautifulSoup(content, 'html.parser')
+    element=soup.find('div',{'class':'game_purchase_price price'})
+    price=element.text.strip()
+    priceNoCurrency=float(price[1:])
 
-price=element.text.strip()
-priceNoCurrency=float(price[1:])
-
-if priceNoCurrency<=20:
-    print("The game is on sale for $20 or less.  You should buy it.")
-else:
-    print("The game is more than you want to spend.  You can wait for a sale.")
+    if gameName=='Street Fighter 6':
+        if priceNoCurrency<=20:
+            print(gameName + ' is less than $20.  You should buy it.')
+        else:
+            print(gameName + " is more than you want to spend.  Wait for a sale.")
+    if gameName=='Monster Hunter Rise':
+        if priceNoCurrency<=20:
+            print(gameName + ' is less than $20.  You should buy it.')
+        else:
+            print(gameName + ' is more than you want to spend.  Wait for a sale.')
+    if gameName=='Kingdoms and Castles':
+        if priceNoCurrency<=10:
+            print(gameName + ' is less than $10.  You should buy it.')
+        else:
+            print(gameName + ' is more than you want to spend.  Wait for a sale.')
