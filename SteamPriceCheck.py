7389a1155a373b1cc367638d413c8062a3044424
timiwashima <52045865+timiwashima@users.noreply.github.com>
2023-06-21 07:55:14 -0700

    Update SteamPriceCheck.py
    Instead of 2 dictionaries, used one dictionary using a game name as a key, with nested dictionaries inside that hold the Steam URL and the price you are looking to pay.

--- SteamPriceCheck.py
+++ SteamPriceCheck.py
@@ -1,39 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 20 13:08:29 2023
+Created on Wed Jun 21 07:06:38 2023
 
 @author: tiwashima
 """
-
 import requests
 from bs4 import BeautifulSoup
 
 games={
-       'Street Fighter 6':'https://store.steampowered.com/app/1364780/Street_Fighter_6/',
-       'Monster Hunter Rise':'https://store.steampowered.com/app/1446780/MONSTER_HUNTER_RISE/',
-       'Kingdoms and Castles':'https://store.steampowered.com/app/569480/Kingdoms_and_Castles/'
+       'Street Fighter 6':
+           {'url':'https://store.steampowered.com/app/1364780/Street_Fighter_6/',
+            'desiredPrice':40},
+       'Monster Hunter Rise':
+           {'url':'https://store.steampowered.com/app/1446780/MONSTER_HUNTER_RISE/',
+            'desiredPrice':20},
+       'Kingdoms and Castles':
+           {'url':'https://store.steampowered.com/app/569480/Kingdoms_and_Castles/',
+            'desiredPrice':10}
        }
 
-prices={
-        'Street Fighter 6':40,
-        'Monster Hunter Rise':20,
-        'Kingdoms and Castles':10
-        }
-
-for game,url in games.items():
-    gameName=game
-    gameURL=url
-    
-    request=requests.get(gameURL)
+for gameTitle,steamGame in games.items():
+    request=requests.get(steamGame['url'])
+    desiredPrice=steamGame['desiredPrice']
     content=request.content
     soup=BeautifulSoup(content, 'html.parser')
     element=soup.find('div',{'class':'game_purchase_price price'})
     price=element.text.strip()
     priceNoCurrency=float(price[1:])
-    
-    for key in prices:
-        if gameName==key:
-            if priceNoCurrency<=prices[gameName]:
-                print(gameName + ' is less than $' + str(prices[gameName]) + '.  You should buy it.')
-            else:
-                print(gameName + ' is more than $' + str(prices[gameName]) + '.  Wait for a sale.')
+    if priceNoCurrency <= desiredPrice:
+        print(gameTitle + ' is $' + str(priceNoCurrency) + '.')
